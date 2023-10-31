60a30bad5e2579e82d5464a473b92ee15b6a33cf
Javier <jav.ruiz1@gmail.com>
2023-10-31 15:06:04 -0500

    Updates

--- app/src/main/java/com/example/mortgagecalchw2/MainActivity.kt
+++ app/src/main/java/com/example/mortgagecalchw2/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.mortgagecalchw2
 
-import android.icu.text.NumberFormat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,14 +13,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -32,6 +29,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.mortgagecalchw2.ui.theme.MortgageCalcHw2Theme
+import java.text.NumberFormat
+import kotlin.math.pow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,66 +53,71 @@ class MainActivity : ComponentActivity() {
 fun MortgageCalcHw2Layout() {
     var amountInput by remember { mutableStateOf("") }
     var tipInput by remember { mutableStateOf("") }
-    var roundUp by remember { mutableStateOf(false) }
 
     val amount = amountInput.toDoubleOrNull() ?: 0.0
     val interestPercent = tipInput.toDoubleOrNull() ?: 0.0
 
     val tip = calculateMortgage(amount, interestPercent)
     Column(
+        modifier = Modifier
+            .padding(40.dp)
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(id = R.string.calculate_mortgage),
             modifier = Modifier
-                .padding(40.dp)
-                .verticalScroll(rememberScrollState()),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text(
-                text = stringResource(R.string.calculate_mortgage),
-                modifier = Modifier
-                    .padding(bottom = 16.dp)
-                    .align(alignment = Alignment.Start)
-            )
-            EditNumberField(
-                label = R.string.mortgage_amount,
-                leadingIcon = R.drawable.null,
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    keyboardType = KeyboardType.Number,
-                    imeAction = ImeAction.Next
-                ),
-                value = amountInput,
-                onValueChange = { amountInput = it},
-                modifier = Modifier
-                    .padding(bottom = 32.dp)
-                    .fillMaxWidth()
-            )
-            EditNumberField(
-                label = R.string.loan_amount,
-                leadingIcon = R.drawable.null,
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    keyboardType = KeyboardType.Number,
-                    imeAction = ImeAction.Done
-                ),
-                value = tipInput,
-                onValueChange = { tipInput = it },
-                modifier = Modifier
-                    .padding(bottom = 32.dp)
-                    .fillMaxWidth()
-            )
+                .padding(bottom = 16.dp)
+                .align(Alignment.Start)
+        )
 
-            Text(
-                text = stringResource(R.string.mortgage_amount),
-                fontWeight = FontWeight.Bold,
-                fontSize = 36.sp
-            )
-            Spacer(modifier = Modifier.height(150.dp))
-        }
+        EditNumberField(
+            modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth(),
+            label = R.string.mortgage_amount,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            value = amountInput
+        ) { amountInput = it }
+
+        EditNumberField(
+            label = R.string.loan_amount,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            value = tipInput
+        ) { tipInput = it }
+
+        Text(
+            text = tip,
+            fontWeight = FontWeight.Bold,
+            fontSize = 36.sp
+        )
+
+        Spacer(modifier = Modifier.height(150.dp))
+    }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun EditNumberField(label: Any, leadingIcon: Any, keyboardOptions: Any, value: String, onValueChange: () -> Unit, modifier: Any) {
-
+fun EditNumberField(
+    modifier: Modifier = Modifier,
+    label: Int,
+    keyboardOptions: KeyboardOptions,
+    value: String,
+    onValueChange: (String) -> Unit
+) {
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        modifier = modifier,
+        label = { Text(text = stringResource(id = label)) },
+        keyboardOptions = keyboardOptions
+    )
 }
-
 
 private fun calculateMortgage(amount: Double, interestPercent: Double = 5.0): String {
     val interest = interestPercent / 100 * amount
